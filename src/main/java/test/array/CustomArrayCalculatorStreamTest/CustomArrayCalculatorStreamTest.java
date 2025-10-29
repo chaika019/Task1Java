481e8eaf66d48358e02414da5e08e4962dbb51ba
Chuiko Egor <egorchuiko7@gmail.com>
2025-10-29 03:40:51 +0300

    Tests for stream service

---
+++ src/main/java/test/array/CustomArrayCalculatorStreamTest/CustomArrayCalculatorStreamTest.java
@@ -0,0 +1,59 @@
+package test.array.CustomArrayCalculatorStreamTest;
+
+import org.junit.jupiter.api.Test;
+import task1.by.chaika19.entity.CustomArray;
+import task1.by.chaika19.exception.CustomArrayException;
+import task1.by.chaika19.service.stream.CustomArrayCalculatorStream;
+import task1.by.chaika19.service.stream.impl.CustomArrayCalculatorStreamImpl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CustomArrayCalculatorStreamTest {
+    private final CustomArrayCalculatorStream calculator = new CustomArrayCalculatorStreamImpl();
+    private final CustomArray testArray = CustomArray.builder()
+            .withArray(new int[]{1,2,3,4,-5,16,7})
+            .build();;
+    private final CustomArray emptyArray = CustomArray.builder()
+            .withArray(new int[]{})
+            .build();
+
+    @Test
+    public void findMin() throws CustomArrayException {
+        int min = calculator.findMin(testArray);
+        assertEquals(-5,min);
+    }
+
+    @Test
+    public void calculator_findMax() throws CustomArrayException {
+        int max = calculator.findMax(testArray);
+        assertEquals(16, max, "max should be 16.");
+    }
+
+    @Test
+    public void calculator_replaceElements() throws CustomArrayException {
+        CustomArray expected = CustomArray.builder()
+                .withArray(new int[]{1,10,3,4,-5,6,7})
+                .build();
+        assertEquals(expected, calculator.replaceElements(testArray, 2, 10));
+    }
+
+    @Test
+    public void calculator_Sum() throws CustomArrayException {
+        assertEquals(28, calculator.calculateSum(testArray));
+    }
+
+    @Test
+    public void calculator_Average() throws CustomArrayException {
+        assertEquals(4, calculator.calculateAverage(testArray));
+    }
+
+    @Test
+    public void calculator_Positives() throws CustomArrayException {
+        assertEquals(6, calculator.countPositives(testArray));
+    }
+
+    @Test
+    public void calculator_Negatives() throws CustomArrayException {
+        assertEquals(1, calculator.countNegatives(testArray));
+    }
+}
